805f16c207093f8147bd194d741118178c177b2d
rohit <rsalvi923@gmail.com>
2018-08-09 09:24:32 +0530

    add server folder an make todos post routes

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+
+var {mongoose}=require('./db/mongoose');
+var {Todo} =require('./models/Todo');
+
+var App=express();
+
+
+App.use(bodyParser.json());
+
+App.post('/todos',(req,res)=>{
+    var newTodo=new Todo({
+        text:req.body.text
+    });
+
+    newTodo.save().then((docs)=>{
+      res.send(docs)
+    },(e)=>{
+       res.status(400).send(e);
+    })
+
+});
+App.listen(3000,()=>{
+    console.log('started port 3000');
+});
